73ccd6337f71fec8c7e3962b001ee54510afa927
Tiraj <tirtharajpramanik555@gmail.com>
2023-07-11 22:06:21 +0530

    move a circle

--- main.c
+++ main.c
@@ -1,22 +1,29 @@
 #include <raylib.h>
 
-#define WIDTH 640
-#define HEIGHT 460
+#define WIDTH 800
+#define HEIGHT 800
+
+void move_ball(Vector2 *ballPos, int speed);
 
 int main(void)
 {
-    const char text[] = "Welcome to Game Window";
+    Vector2 ballPos = {WIDTH / 2, HEIGHT / 2};
+    int speed = 300;
 
     InitWindow(WIDTH, HEIGHT, "Game Window");
-    SetTargetFPS(30);
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        // Update
+        move_ball(&ballPos, speed);
+
+        // Draw
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText(text, WIDTH / 2 - 8 * (int)sizeof(text), HEIGHT / 2 - 32, 32, LIGHTGRAY);
+        DrawCircle(ballPos.x, ballPos.y, 10, GRAY);
 
         EndDrawing();
     }
@@ -24,3 +31,44 @@ int main(void)
     CloseWindow();
     return 0;
 }
+
+void move_ball(Vector2 *ballPos, int speed)
+{
+    short kw, ks, ka, kd;
+    kw = ks = ka = kd = 0;
+    if (IsKeyDown(KEY_W))
+        kw = 1;
+    if (IsKeyDown(KEY_S))
+        ks = 1;
+    if (IsKeyDown(KEY_A))
+        ka = 1;
+    if (IsKeyDown(KEY_D))
+        kd = 1;
+    const int p = kw + ks + ka + kd;
+
+    if (p)
+    {
+        float delta = speed * GetFrameTime() / p;
+        if (kw)
+            ballPos->y -= delta;
+        if (ks)
+            ballPos->y += delta;
+        if (ka)
+            ballPos->x -= delta;
+        if (kd)
+            ballPos->x += delta;
+    }
+}
+
+// void move_ball(Vector2 *ballPos, int speed)
+// {
+//     float delta = speed * GetFrameTime();
+//     if (IsKeyDown(KEY_W))
+//         ballPos->y -= delta;
+//     if (IsKeyDown(KEY_S))
+//         ballPos->y += delta;
+//     if (IsKeyDown(KEY_A))
+//         ballPos->x -= delta;
+//     if (IsKeyDown(KEY_D))
+//         ballPos->x += delta;
+// }
